2b823f10624fbecd7681e67a02e12883d3baf89e
Daniel Walker <nickeldan@gmail.com>
2022-01-04 23:00:41 -0500

    added getopt to main.c

--- hallmonitor/main.c
+++ hallmonitor/main.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -8,6 +9,12 @@
 #include <hamo/journal.h>
 
 #define TCP_ACK_FLAG 0x10
+
+static void
+usage(const char *exec)
+{
+    printf("Usage: %s [-d <network device>]* [-w <whitelist file>]* [-v] [-h]\n", exec);
+}
 
 static void
 printRecord(void *user, const hamoRecord *record)
@@ -35,34 +42,63 @@ printRecord(void *user, const hamoRecord *record)
 int
 main(int argc, char **argv)
 {
-    int ret;
-    char *device;
+    int ret, option;
+    vasqLogLevel_t level = VASQ_LL_INFO;
+    const char *format_string = "%t [%L]%_ M\n";
+    void *item;
+    hamoArray devices = HAMO_ARRAY(const char *);
+    hamoArray whitelist_entries = HAMO_ARRAY(hamoWhitelistEntry);
     hamoDispatcher dispatcher = HAMO_DISPATCHER_INIT;
     hamoJournaler journaler = {.func = printRecord, .user = NULL};
 
-    if (argc < 2) {
-        fprintf(stderr, "Missing device name\n");
-        return HAMO_RET_USAGE;
+    while ((option = getopt(argc, argv, "d:w:vh")) != -1) {
+        switch (option) {
+        case 'v':
+            level = VASQ_LL_DEBUG;
+            format_string = "%t [%L]%_ %f:%l: %M\n";
+            break;
+
+        case 'd':
+        case 'w': break;
+
+        case 'h': usage(argv[0]); return HAMO_RET_OK;
+
+        default: usage(argv[0]); return HAMO_RET_USAGE;
+        }
     }
-    device = argv[1];
 
-#ifndef LL_USE
-#ifdef DEBUG
-#define LL_USE VASQ_LL_DEBUG
-#else
-#define LL_USE VASQ_LL_INFO
-#endif
-#endif
-
-#ifdef DEBUG
-#define LOGGER_FORMAT "%t [%L]%_ %f:%l: %M\n"
-#else
-#define LOGGER_FORMAT "%t [%L]%_ %M\n"
-#endif
-    ret = vasqLoggerCreate(STDOUT_FILENO, LL_USE, LOGGER_FORMAT, NULL, &hamo_logger);
+    ret = vasqLoggerCreate(STDOUT_FILENO, level, format_string, NULL, &hamo_logger);
     if (ret != VASQ_RET_OK) {
         fprintf(stderr, "vasqLoggerCreate: %s\n", vasqErrorString(ret));
         return HAMO_RET_OUT_OF_MEMORY;
+    }
+
+    optind = 1;
+    while ((option = getopt(argc, argv, "d:w:vh")) != -1) {
+        switch (option) {
+            FILE *f;
+
+        case 'd':
+            ret = hamoArrayAppend(&devices, &optarg);
+            if (ret != HAMO_RET_OK) {
+                goto done;
+            }
+            break;
+
+        case 'w':
+            f = fopen(optarg, "r");
+            if (!f) {
+                VASQ_PERROR(hamo_logger, "Could not read from whitelist file", errno);
+                ret = HAMO_RET_BAD_WHITELIST;
+                goto done;
+            }
+            ret = hamoWhitelistLoad(f, &whitelist_entries);
+            fclose(f);
+            if (ret != HAMO_RET_OK) {
+                goto done;
+            }
+            break;
+        }
     }
 
     VASQ_INFO(hamo_logger, "Running Hallmonitor %s", HAMO_VERSION);
@@ -72,10 +108,23 @@ main(int argc, char **argv)
         goto done;
     }
 
-    ret = hamoPcapAdd(&dispatcher.handles, device, NULL);
-    if (ret != HAMO_RET_OK) {
-        goto done;
+    if (devices.length > 0) {
+        ARRAY_FOR_EACH(&devices, item)
+        {
+            ret = hamoPcapAdd(&dispatcher.handles, *(const char **)item, &whitelist_entries);
+            if (ret != HAMO_RET_OK) {
+                goto done;
+            }
+        }
+        hamoArrayFree(&devices);
     }
+    else {
+        ret = hamoPcapAdd(&dispatcher.handles, "any", &whitelist_entries);
+        if (ret != HAMO_RET_OK) {
+            goto done;
+        }
+    }
+    hamoArrayFree(&whitelist_entries);
 
     VASQ_INFO(hamo_logger, "Beginning packet capturing");
 
@@ -83,6 +132,8 @@ main(int argc, char **argv)
 
 done:
     hamoDispatcherFree(&dispatcher);
+    hamoArrayFree(&devices);
+    hamoArrayFree(&whitelist_entries);
     vasqLoggerFree(hamo_logger);
 
     return ret;
