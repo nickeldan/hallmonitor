d3d686acd27dc6eea9e7a63ccbdf164ab54ab397
Daniel Walker <nickeldan@gmail.com>
2022-04-25 15:44:47 -0400

    added detection of the relevant process to the hamo executable (todo: add reap package to submodules)

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ debug ?= no
 CLEAN_TARGETS :=
 DEPS_FILES :=
 
-CFLAGS := -std=gnu99 -fdiagnostics-color -Wall -Wextra
+CFLAGS := -std=gnu11 -fdiagnostics-color -Wall -Wextra
 ifeq ($(debug),yes)
 	CFLAGS += -O0 -g -DDEBUG
 else
@@ -15,15 +15,24 @@ all: hamo
 HAMO_DIR := .
 include make.mk
 
+REAP_DIR := packages/reap
+include $(REAP_DIR)/make.mk
+
+MAIN_HEADER_FILES := $(wildcard hallmonitor/*.h)
+MAIN_SOURCE_FILES := $(wildcard hallmonitor/*.c)
+MAIN_OBJECT_FILES := $(patsubst %.c,%.o,$(MAIN_SOURCE_FILES))
+
 MAIN_DEPS_FILE := main_deps.mk
 DEPS_FILES += $(MAIN_DEPS_FILE)
 
 ifneq ($(MAKECMDGOALS),clean)
 
-$(MAIN_DEPS_FILE): hallmonitor/main.c $(HAMO_HEADER_FILES) $(HAMO_EXTERNAL_HEADER_FILES)
+$(MAIN_DEPS_FILE): $(MAIN_SOURCE_FILES) $(MAIN_HEADER_FILES) $(REAP_HEADER_FILES) $(HAMO_HEADER_FILES)
 	rm -f $@
-	echo "hallmonitor/`$(CC) $(HAMO_INCLUDE_FLAGS) -MM $<`" >> $@
-	echo '\t$$(CC) $$(CFLAGS) -fpic -ffunction-sections $(HAMO_INCLUDE_FLAGS) -c $$< -o $$@' >> $@
+	for file in $(MAIN_SOURCE_FILES); do \
+	    echo "hallmonitor/`$(CC) $(HAMO_INCLUDE_FLAGS) $(REAP_INCLUDE_FLAGS) -MM $$file`" >> $@ && \
+	    echo '\t$$(CC) $$(CFLAGS) -fpic -ffunction-sections $(HAMO_INCLUDE_FLAGS) $(REAP_INCLUDE_FLAGS) -c $$< -o $$@' >> $@; \
+	done
 include $(MAIN_DEPS_FILE)
 
 endif
@@ -32,8 +41,8 @@ endif
 
 libs: $(HAMO_SHARED_LIBRARY) $(HAMO_STATIC_LIBRARY)
 
-hamo: hallmonitor/main.o $(HAMO_STATIC_LIBRARY) $(VASQ_STATIC_LIBRARY)
-	$(CC) $(CFLAGS) $(HAMO_INCLUDE_FLAGS) -o $@ $^ -lpcap
+hamo: $(MAIN_OBJECT_FILES) $(HAMO_STATIC_LIBRARY) $(REAP_STATIC_LIBRARY) $(VASQ_STATIC_LIBRARY)
+	$(CC) $(CFLAGS) $(HAMO_INCLUDE_FLAGS) -o $@ $^ -lpcap -pthread
 
 clean: $(CLEAN_TARGETS)
-	@rm -f hamo hallmonitor/main.o $(DEPS_FILES)
+	@rm -f hamo $(MAIN_OBJECT_FILES) $(DEPS_FILES)
--- hallmonitor/main.c
+++ hallmonitor/main.c
@@ -9,6 +9,8 @@
 #include <hamo/journal.h>
 #include <hamo/whitelist.h>
 
+#include "proc_search.h"
+
 static volatile sig_atomic_t signal_caught;
 
 static void
@@ -72,15 +74,16 @@ main(int argc, char **argv)
     hamoArray devices = HAMO_ARRAY(const char *);
     hamoArray whitelist_entries = HAMO_ARRAY(hamoWhitelistEntry);
     hamoDispatcher dispatcher = HAMO_DISPATCHER_INIT;
-    hamoJournaler journaler = {.func = printRecord, .user = NULL};
+    hamoJournaler journaler = {.func = printRecord, .user = NULL},
+                  proc_journaler = {.func = startProcSearch, .user = NULL};
 
 #ifdef VASQ_NO_LOGGING
 
-#define GETOPT_FORMAT "d:w:h"
+#define GETOPT_FORMAT "d:w:fh"
 
 #else
 
-#define GETOPT_FORMAT "d:w:vh"
+#define GETOPT_FORMAT "d:w:fvh"
 
     vasqLogLevel_t level = VASQ_LL_INFO;
     const char *format_string = "%t [%L]%_ %M\n";
@@ -89,11 +92,12 @@ main(int argc, char **argv)
         switch (option) {
         case 'v':
             level = VASQ_LL_DEBUG;
-            format_string = "%t [%L]%_ %f:%l: %M\n";
+            format_string = "%T: %t [%L]%_ %F:%f:%l: %M\n";
             break;
 
         case 'd':
-        case 'w': break;
+        case 'w':
+        case 'f': break;
 
         case 'h': usage(argv[0]); return HAMO_RET_OK;
 
@@ -112,6 +116,11 @@ main(int argc, char **argv)
     VASQ_INFO(hamo_logger, "Running Hall Monitor %s", HAMO_VERSION);
 
 #endif  // VASQ_NO_LOGGING
+
+    ret = hamoArrayAppend(&dispatcher.journalers, &journaler);
+    if (ret != HAMO_RET_OK) {
+        goto done;
+    }
 
     while ((option = getopt(argc, argv, GETOPT_FORMAT)) != -1) {
         switch (option) {
@@ -138,6 +147,13 @@ main(int argc, char **argv)
             }
             break;
 
+        case 'f':
+            ret = hamoArrayAppend(&dispatcher.journalers, &proc_journaler);
+            if (ret != HAMO_RET_OK) {
+                goto done;
+            }
+            break;
+
 #ifdef VASQ_NO_LOGGING
 
         case 'h':
@@ -152,11 +168,6 @@ main(int argc, char **argv)
 
 #endif  // VASQ_NO_LOGGING
         }
-    }
-
-    ret = hamoArrayAppend(&dispatcher.journalers, &journaler);
-    if (ret != HAMO_RET_OK) {
-        goto done;
     }
 
     if (devices.length > 0) {
